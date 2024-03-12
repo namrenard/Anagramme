f9b02cdbaf9573d7acff172a2211a486db69b6c9
nrenard <nicolas.renard@etudiant.univ-brest.fr>
2024-03-13 00:18:35 +0100

    add v0.5.0

--- README.md
+++ README.md
@@ -1,24 +1,39 @@
-# Anagram : le générateur de mots aléatoires
+# Anagram : Word Random Generator
 
 ![logo_black](https://user-images.githubusercontent.com/87765071/127388530-5cf0ea97-e660-4811-bd79-7a5f18c4a9a0.png)
 
+## About
 
-FR: Un programme simple en python pour trouver des anagrammes à partir d'un mot.
-Peut servir de programme pour trouver un pseudonyme en variant les combinaisons.
-Différents modes possibles : terminal app, mobile 
-
-ENG: A Small program in python to find some anagramme from one word.
-Could be useful in order to find a pseudo for example with the letter's shuffle on the programm.
-Different version avalaible : terminal, mobile app
-
-Releases --
+A small program to find some anagram from one word.
+Could be useful in order to find a 'pseudonyme'.
+The program gave the possibility to generate one word or a list of words.
+For example :  hello -> loleh, llohe, holel, lohel etc..
 
 
-V1 : gui interface with Kivi generated.
+### Releases :
 
-current : V0.4.1 : fixed bug in checking the word by user. use regex to solve it.
-V0.4 - fixed bug in reloading the program a second time, merge reloading program inside multiple function
-V0.3 : add reloading function.
-V0.2 : add function to check str or int.
-V0.1 : add multiple shuffle function
-V0 : mode : terminal
+current : V0.5.0 
+
+* v0.5.0 : 
+  * refactoring code 
+  * add random default letters if no word is given
+* V0.4.1 : 
+  * fixed bug in checking the word by user with regex to solve it.
+* V0.4.0 : 
+  * fixed bug in reloading the program a second time, merge reloading program inside multiple function
+* V0.3.0 : 
+  * add reloading function.
+* V0.2.0 : 
+  * add function to check str or int.
+* V0.1.0 : 
+  * add multiple shuffle function
+* V0.0.1 : 
+  * 1rst release in terminal
+
+### Features :
+* [ ] Gui
+* [x] random word
+
+### Author : 
+
+namrenard
--- src/main.py
+++ src/main.py
@@ -1,98 +1,86 @@
-'''
-1) trouver l'anagramme d'un mot  = ok
-2) verifier que c'est un mot et non des chiffres = bug car si les deux mixer programme continue.
-3) fonction pour relancer avec le même mot ou choisir un nouveau = ok
-3.1) fonction pour un shuffle de x fois le mot  = ok
-4) interface gui : kivi md pour mobile
-4.1) faire un input ou text pour le mot, un bouton pour le shuffle et un bouton pour le nombre de recherche
-4.2) mettre un logo du jeu.
-4.3) mettre de la couleur en bandeau et arrière plan de l'appli.
-4.4) bouton pour relancer le programme en effacant les données
-5) compiler sous apk
-'''
-
+import random
+import string
+import sys
 from random import shuffle
 import re
 
-# GUI kivi MD
-
-'''class AnagramApp(MDApp):
-    def build(self):
-        screen = Screen()
-        screen.add_widget(
-            MDRectangleFlatButton(
-                text="Click here to Shuffle",
-                pos_hint={"center_x": 0.5, "center_y": 0.5},
-            )
-        )
-        return screen'''
+from typing import List
 
 
+class Anagram:
 
-
-class Word:
-
-    def __init__(self, word, token=0):
-        self.word = word
+    def __init__(self, word: str, token: int = 0):
+        self.word = word if Anagram._check_word(word) else Anagram._random_word()
         self.token = token
 
-    # function shuffle for a word
-    def anagramme(self):
+    @staticmethod
+    def _random_word() -> str:
+        """
+        Method to generate a list of letter if no world is given.
+
+        :return:  a block of letter
+        """
+        return "".join(random.sample(list(string.ascii_lowercase), k=random.randint(1, 26)))
+
+    @staticmethod
+    def _check_word(word: str) -> bool:
+        """
+        Static method to check that the word in parameter is word and not a pure digit or
+        a mixture of letters and digit.
+
+        :param word: the word given in parameter
+        :return: True if the word is a pure string with letters
+        """
+        check = bool(re.search(r'\d', word))
+        return True if not check else False
+
+    def _shuffle_word(self) -> str:
+        """
+        Method to shuffle the letter of a word and make a new word.
+        :return: un new word
+        """
         a = list(self.word)
         shuffle(a)
         return "".join(a)
 
     # choice of shuffle
-    def anagramme_multiple(self, token):
-        nombre_de_recherche = ''
-        y = 1
-        while nombre_de_recherche == '' or nombre_de_recherche.isdigit() == False:
-            nombre_de_recherche = input("Choisissez le nombre d'anagramme à générer:")
-        nombre_de_recherche_int = int(nombre_de_recherche)
-        print(f"Voici les résultats pour {nombre_de_recherche} anagrammes du mot ({self.word}):")
-        print()
-        while y <= nombre_de_recherche_int:
-            print("".join(self.anagramme()))
-            y = y+1
-        if token == 0:
-            self.relance_anagramme()
+    def start_research(self) -> List[str]:
+        """
+        Method to generate a list of new words.
+        :return: a list of new words
+        """
+        anagrams = []
+        shuffle_number = None
+        counter = 0
+        while shuffle_number is None or shuffle_number.isdigit() is False:
+            shuffle_number = input("[?] Give a number of run you want : >> ")
+        research_int = int(shuffle_number)
+        while not counter == research_int:
+            result = self._shuffle_word()
+            if result != self.word:
+                anagrams.append(result)
+            counter += 1
+        if self.token == 0:
+            self.reload_search()
         else:
-            exit()
+            return anagrams
 
     # again a shuffle or shutdown the program?
-    def relance_anagramme(self):
-        question = input("Relancez le générateur avec le même mot ? o/n")
-        if question.lower() == "o":
-            self.anagramme_multiple(1)
-        elif question.lower() == "n":
-            print("Ok. Programme Terminé")
+    def reload_search(self) -> None:
+        """Method to restart a shuffle with the same word"""
+        query = input(">> Start again the search of an anagram ?! >> [o/n]")
+        if query.lower() == "o":
+            self.start_research()
         else:
-            print("Vous n'avez pas donné de réponse valide. Programme Terminé")
-            self.relance_anagramme()
-
-
-    # check if it's a word or number // miss to block the mixture of word+number
-    def verification_anagramme(self):
-        m = self.word
-        bool(re.search(r'\d', m))
-        if True:
-            print(f"Erreur, vous n'avez pas inscrit un mot, recommencez s'il vous plait.")
-            self.word = input("Quel est votre mot ? :")
-            self.verification_anagramme()
-        else:
-            return
+            sys.exit(0)
 
 
 # ---------------------------program-----------------------------
-'''if __name__ == '__main__':
-    AnagramApp().run()'''
-print("Bonjour, ceci est un petit programme pour trouver un anagramme aléatoirement.")
+print("Hello, this is a small program to find an anagram randomly.")
 print()
 mot = ''
 while mot == '':
-    mot = input("Veuillez donner un mot :")
+    mot = input("[?] Give a word, please. \n(if none, a default list of letters will be generated.\n >> ")
 
-mot_str = Word(mot)
-mot_str.verification_anagramme()
-mot_str.anagramme_multiple(0)
-# mot_str.relance_anagramme()
+mot_str = Anagram(mot, 0)
+mot_str.start_research()
